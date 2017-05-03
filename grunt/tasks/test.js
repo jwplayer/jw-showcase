936c8e0095e4bacc7ee901fb9e397cabb19f1fd0
Christiaan Scheermeijer <christiaan@videodock.com>
2017-05-03 17:01:11 +0200

    Update test task in package.json and grunt config

--- grunt/tasks/test.js
+++ grunt/tasks/test.js
@@ -1,11 +1,6 @@
 module.exports = function (grunt) {
 
     grunt.registerTask('test:unit', [
-        'clean:server',
-        'compass',
-        'ngtemplates:server',
-        'template:server',
-        'connect:test',
         'karma'
     ]);
 
