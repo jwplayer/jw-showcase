d543b4a7f96752181a388841f7d33095114b4d4a
Pieter Janssen <pjanssen@jwplayer.com>
2017-11-02 16:06:10 +0100

    Clean up Protractor configs

--- protractor.conf.browserstack.js
+++ protractor.conf.browserstack.js
@@ -1,45 +1,15 @@
-var { extendConfig } = require('./protractor.conf.base');
+var browserstack = require('browserstack-local');
+var pkg = require('./package.json');
+var env = process.env;
+var local;
 
-var browserstack = require('browserstack-local'),
-    pkg          = require('./package.json'),
-    env          = process.env,
-    local;
+var extendConfig = require('./protractor/_base');
 
 exports.config = extendConfig(
-    function(baseConfig, createCapabilities) {
-        /**
-         * Create BrowserStack capabilities
-         *
-         * @param   {Object}    capabilities    OS capabilities
-         * @param   {Array}     [tags]          Optional cucumber tags
-         *
-         * @returns {Object} Capabilities
-         */
-        function createBrowserstackCapabilities (capabilities, tags) {
-            capabilities = createCapabilities(capabilities, tags);
-
-            capabilities['browserstack.user']  = env.BROWSERSTACK_USER || env.BS_USERNAME;
-            capabilities['browserstack.key']   = env.BROWSERSTACK_KEY || env.BS_AUTHKEY;
-            capabilities['browserstack.local'] = true;
-
-            // Selenium 3.4.0 does not work great with IE11
-            if (capabilities.browserName !== 'internet explorer') {
-                capabilities['browserstack.selenium_version'] = '3.4.0';
-            }
-
-            capabilities.project = pkg.name;
-            capabilities.build   = env.BROWSERSTACK_BUILD || pkg.version;
-
-            if (capabilities.browserName === 'internet explorer') {
-                capabilities.ignoreProtectedModeSettings = true;
-            }
-
-            return capabilities;
-        }
-
+    function() {
         return {
-            allScriptsTimeout: 30000,
-            getPageTimeout:    30000,
+            allScriptsTimeout: 60000,
+            getPageTimeout:    60000,
 
             maxSessions: 2,
 
@@ -64,78 +34,29 @@ exports.config = extendConfig(
                 return new Promise(function (resolve) {
                     local.stop(resolve);
                 });
-            },
-
-            multiCapabilities: [
-
-                // //
-                // // OS X
-                // //
-
-                // latest Firefox
-                createBrowserstackCapabilities({
-                    'browserName': 'firefox',
-                    'os':          'OS X',
-                    'os_version':  'Sierra'
-                }, ['@desktop']),
-
-                // latest Chrome
-                createBrowserstackCapabilities({
-                    'browserName': 'chrome',
-                    'os':          'OS X',
-                    'os_version':  'Sierra'
-                }, ['@desktop']),
-
-                // latest Safari
-                createBrowserstackCapabilities({
-                    'browserName': 'safari',
-                    'os':          'OS X',
-                    'os_version':  'Sierra'
-                }, ['@desktop']),
-
-                // Safari 9.1
-                createBrowserstackCapabilities({
-                    'browserName': 'safari',
-                    'version':     '9.1',
-                    'os':          'OS X',
-                    'os_version':  'El Capitan'
-                }, ['@desktop']),
-
-                //
-                // Windows
-                //
-
-                // Internet Explorer 11
-                createBrowserstackCapabilities({
-                    'browserName': 'internet explorer',
-                    'version':     '11',
-                    'os':          'WINDOWS',
-                    'os_version':  '10'
-                }, ['@desktop']),
-
-                // latest Chrome
-                createBrowserstackCapabilities({
-                    'browserName': 'chrome',
-                    'os':          'WINDOWS',
-                    'os_version':  '10'
-                }, ['@desktop']),
-
-                // latest Firefox
-                createBrowserstackCapabilities({
-                    'browserName': 'firefox',
-                    'os':          'WINDOWS',
-                    'os_version':  '10'
-                }, ['@desktop']),
-
-                // last Edge
-                createBrowserstackCapabilities({
-                    'browserName': 'edge',
-                    'os':          'WINDOWS',
-                    'os_version':  '10'
-                }, ['@desktop'])
-
-            ]
+            }
         };
     },
-    './test/reports/browserstack'
+
+    './test/reports/browserstack',
+
+    function (capabilities) {
+        capabilities['browserstack.user']  = env.BROWSERSTACK_USER || env.BS_USERNAME;
+        capabilities['browserstack.key']   = env.BROWSERSTACK_KEY || env.BS_AUTHKEY;
+        capabilities['browserstack.local'] = true;
+
+        // Selenium 3.4.0 does not work great with IE11
+        if (capabilities.browserName !== 'internet explorer') {
+            capabilities['browserstack.selenium_version'] = '3.4.0';
+        }
+
+        capabilities.project = pkg.name;
+        capabilities.build   = env.BROWSERSTACK_BUILD || pkg.version;
+
+        if (capabilities.browserName === 'internet explorer') {
+            capabilities.ignoreProtectedModeSettings = true;
+        }
+
+        return capabilities;
+    }
 );
--- protractor.conf.local.js
+++ protractor.conf.local.js
@@ -1,31 +1,23 @@
 var fs = require('fs');
 
-var { extendConfig, argv } = require('./protractor.conf.base');
+var extendConfig = require('./protractor/_base');
 
 var LOCAL_IP = fs.readFileSync('./LOCAL_IP', 'utf8').trim() || 'localhost';
 
-var localConfig = {
-    baseUrl: 'http://' + LOCAL_IP +':9001',
+exports.config = extendConfig(
+    function() {
+        return {
+            baseUrl: 'http://' + LOCAL_IP +':9001',
 
-    maxSessions: 10,
+            maxSessions: 10,
 
-    seleniumAddress: 'http://localhost:4444/wd/hub',
+            seleniumAddress: 'http://localhost:4444/wd/hub',
 
-    params: {
-        envType: 'local'
-    }
-};
-
-module.exports = {
-    // hijack callback and extendConfig to add own localConfig
-    extendConfig: function(callback) {
-        return extendConfig(
-            function(baseConfig, createCapabilities) {
-                var newBaseConfig = Object.assign({}, baseConfig, localConfig);
-                return Object.assign({}, localConfig, callback(newBaseConfig, createCapabilities));
-            },
-            './test/reports/local/'
-        );
+            params: {
+                envType: 'local'
+            }
+        };
     },
-    argv
-};
+
+    './test/reports/local/'
+);
