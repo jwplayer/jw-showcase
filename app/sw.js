5f5709486eda33524fdeedff5f4a8e914e7056fa
Pieter Janssen <4687577+pajter@users.noreply.github.com>
2018-05-17 14:02:49 +0200

    Update sw precache files

--- app/sw.js
+++ app/sw.js
@@ -9,15 +9,23 @@ toolbox.options.cache.name = 'jw-showcase';
 
 toolbox.precache([
     '/',
-    'config.json',
-    'manifest.json',
-    'favicon.ico',
-    'styles/main.css',
-    'fonts/icons.eot',
-    'fonts/icons.svg',
-    'fonts/icons.ttf',
-    'fonts/icons.woff',
-    /* inject:scripts */
+    '/index.html',
+    '/config.json',
+    '/manifest.json',
+
+    // Scripts
+    'application.js',
+    'bridge.js',
+    'scripts.js',
+    'vendor.1.js',
+    'vendor.2.js',
+
+    // Styles
+    '../styles/main.css',
+    '../styles/vendor.css',
+
+    // Fonts
+    '../fonts/icons.ttf'
 ]);
 
 toolbox.router.get('/(.*)', toolbox.networkFirst, { origin: 'content.jwplatform.com' });
@@ -42,15 +50,13 @@ toolbox.router.get('/(.*)', toolbox.cacheFirst);
 toolbox.router.default = toolbox.networkFirst;
 
 self.onmessage = function (e) {
-
     var data = JSON.parse(e.data);
 
     // needs to be online!
     if (navigator.onLine) {
         if (data.type === 'prefetchPlayer') {
             prefetchPlayer(data.repo);
-        }
-        else if (data.type === 'prefetchConfig') {
+        } else if (data.type === 'prefetchConfig') {
             prefetchConfig(data.config);
         }
     }
@@ -63,23 +69,15 @@ self.addEventListener('activate', function (event) {
     return event.waitUntil(self.clients.claim());
 });
 
-/**
- * @todo this should happen sooner, find a way to obtain repo
- * @param repo
- */
 function prefetchPlayer (repo) {
-
     toolbox.cache(repo + 'provider.html5.js');
     toolbox.cache(repo + 'provider.cast.js');
     toolbox.cache(repo + 'jwplayer.controls.js');
     toolbox.cache(repo + 'jwpsrv.js');
     toolbox.cache(repo + 'related.js');
-    toolbox.cache(repo + 'jw-icons.woff');
-    toolbox.cache(repo + 'jw-icons.ttf');
 }
 
 function prefetchConfig (config) {
-
     var base    = config.contentService,
         content = config.content || [];
 
